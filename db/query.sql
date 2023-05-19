dc29528d9e8d3e67bab52ffb2a838e6e5c8afabe
Aubrey Jones <aubrey_jones07@hotmail.com>
2023-05-18 23:27:40 -0600

    joined tables with a query

--- db/query.sql
+++ db/query.sql
@@ -1,1 +1,5 @@
-SELECT clothing.clothing_title AS 
+SELECT clothing.clothing_title AS clothing, clothingDescriptions.clothingDescription
+FROM clothingDescriptions
+LEFT JOIN clothing
+ON clothingDescriptions.clothing_id = clothing.id     
+ORDER BY clothing.clothing_title;
