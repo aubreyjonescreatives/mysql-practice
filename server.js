fad9c7863e36c02dfb2c72b29d2d0169281acbd2
Aubrey Jones <aubrey_jones07@hotmail.com>
2023-05-18 23:46:41 -0600

    added some node packages and .env variables

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const mysql = require('mysql2'); 
 
+const MYSQLP = process.env.MYSQL_PASSWORD; 
 
 const PORT = process.env.PORT || 5000; 
 const app = express(); 
@@ -12,7 +13,7 @@ const dbConnection = mysql.createConnection(
     {
         host: 'localhost',
         user: 'root', 
-        password: ' ', 
+        password: `${MYSQLP}`, 
         database: 'ecommerce_db'
     },
     console.log(`We're successfully connected to the ecommerce_db database!!!`)
