7a92d0fdb541296fe2e2b87cbcd6f7b894a12051
Aubrey Jones <aubrey_jones07@hotmail.com>
2023-05-18 23:37:01 -0600

    created basic server file for sql connection

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+
+const mysql = require('mysql2'); 
+
+
+const PORT = process.env.PORT || 5000; 
+const app = express(); 
+
+
+
+const dbConnection = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root', 
+        password: ' ', 
+        database: 'ecommerce_db'
+    },
+    console.log(`We're successfully connected to the ecommerce_db database!!!`)
+)
+
+
+
+app.use((req, res) => {
+    res.status(404).end(); 
+}); 
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server now listening with port ${PORT}`); 
+});
