e079593f9e71f78c3fb645f598023a0f0183bd3d
Aubrey Jones <aubrey_jones07@hotmail.com>
2023-05-18 23:10:08 -0600

    added some sql seed data

---
+++ db/seeds.sql
@@ -0,0 +1,15 @@
+INSERT INTO clothing (clothing_title)
+VALUES ("Tops"), 
+        ("Bottoms"), 
+        ("Dresses"), 
+        ("Outerwear"),
+        ("Shoes"),
+        ("Accessories");
+
+INSERT INTO titleItem (clothing_id, titleItem)
+VALUES (1, "Tops for all occassions."),
+        (2, "Find fitting Bottoms for everyone."),
+        (3, "Seize every moment."),
+        (4, "Feeling cold? Look at the latest styles."),
+        (5, "From lounging around to standing tall. We have them all."),
+        (6, "Add extra flare with these designs."),
